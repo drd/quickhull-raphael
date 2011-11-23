a8e219acd4c25413316e0d4937fc99845f32830c
Gregory Haynes <greg@greghaynes.net>
2011-11-22 17:46:00 -0800

    Lots more debug info for quickhull, point_left_of checks correct side

--- quickhull.js
+++ quickhull.js
@@ -10,28 +10,37 @@ function quickhull(points_list, chord_finder) {
 		return [min, max];
 	}
 
-	function triangle_part(points, p, q) {
+	function triangle_part(points, p, q, ch_points) {
 		if(points.length == 0)
 			return;
-		var ch_points = []
+		if(!ch_points)
+			var ch_points = []
 
 		var farthest = _.max(points, function(point) {
-			return geometry.dist_point_from_line(point, this[0], this[1]);
-		}, chord);
+			return geometry.dist_point_from_line(point, p, q);
+		}, {'p': p, 'q': q});
+		console.log('p: ', p);
+		console.log('q: ', q);
+		console.log('farthest: ', farthest);
 		
 		if(!geometry.points_are_equal(p, farthest) && !geometry.points_are_equal(q, farthest))
 			ch_points.push(farthest);
+		console.log('hull list: ', ch_points)
 
 		var s1_points = _.filter(points, function(point) {
-			return geometry.point_left_of([p, farthest], point);
+			if(geometry.points_are_equal(point, p) || geometry.points_are_equal(point, farthest))
+				return false;
+			return geometry.point_left_of_or_on([farthest, p], point);
 		}, {'p': p, 'farthest': farthest});
+
 		var s2_points = _.filter(points, function(point) {
-			return geometry.point_left_of([farthest, q], point);
+			if(geometry.points_are_equal(point, q) || geometry.points_are_equal(point, farthest))
+				return false;
+			return geometry.point_left_of_or_on([q, farthest], point);
 		}, {'q': q, 'farthest': farthest});
 
-		ch_points.concat(triangle_part(s1_points, p, farthest));
-		ch_points.concat(triangle_part(s2_points, farthest, q));
-		console.log(ch_points);
+		triangle_part(s1_points, p, farthest, ch_points);
+		triangle_part(s2_points, farthest, q, ch_points);
 
 		return ch_points;
 	}
@@ -42,10 +51,10 @@ function quickhull(points_list, chord_finder) {
 
 	var hull_points = [].concat(chord);
 
-	var upper = _.filter(points_list, function(point) {
+	var lower = _.filter(points_list, function(point) {
 		return geometry.point_left_of(chord, point);
 	}, {'chord': chord});
-	var lower = _.filter(points_list, function(point) {
+	var upper = _.filter(points_list, function(point) {
 		return geometry.point_right_of(chord, point);
 	}, {'chord': chord});
 	console.log('Dataset: ', points_list);
@@ -53,8 +62,13 @@ function quickhull(points_list, chord_finder) {
 	console.log('upper: ', upper);
 	console.log('lower: ', lower);
 
-	hull_points.concat(triangle_part(upper, chord[0], chord[1]));
-	hull_points.concat(triangle_part(lower, chord[1], chord[0]));
+	console.log('');
+	console.log('Handling upper');
+	triangle_part(upper, chord[0], chord[1], hull_points);
+
+	console.log('');
+	console.log('Handling lower');
+	triangle_part(lower, chord[1], chord[0], hull_points);
 	return hull_points;
 }
 
